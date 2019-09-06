e78ffefd82dcb7464215d6a1960796de3bc9fa56
Angel Alvarez <angel.alvarez@quadit.mx>
2019-09-06 12:59:06 -0500

    1 hora 6 de septiembre

--- models/models.py
+++ models/models.py
@@ -3,27 +3,30 @@
 from odoo import models, fields, api,exceptions
 
 class res_partner(models.Model):
-	_name ='res.partner'
-	_inherit = 'res.partner'
-	#is_school = fields.Boolean('Escuela')
-	company_type = fields.Selection(selection_add=[('is_school', 'Escuela'),('student','Estudiante')])
-    student_id = fields.Many2one('academy.student', 'Estudiante')
+    _name ='res.partner'
+    _inherit = 'res.partner'
+    #is_school = fields.Boolean('Escuela')
+    company_type = fields.Selection(selection_add=[('is_school', 'Escuela'),('student','Estudiante')])
+    student = fields.Many2one(
+        'academy.student', 
+        'Estudiante')
 
 class academy_student(models.Model):
     _name = 'academy.student'
     _description = 'Modelo de formulario para estudiantes'
     name = fields.Char('Nombre',size=128,required=True)
-    last_name = fields.Char('Apellido',size=128)
+    last_name = fields.Char('Apellido',size=128,required=True)
     photo = fields.Binary('Fotografia')
     create_date = fields.Datetime('Fecha de creacion',readonly=True)
     notes = fields.Html('Comentarios')
     active = fields.Boolean('Activado')
     state = fields.Selection([('draf','Documento Borrador'),
-    						  ('progress','Progreso'),
-    						  ('done','Egresado'),], 'Estado')
+                              ('progress','Progreso'),
+                              ('done','Egresado'),], 'Estado')
     
     age = fields.Integer('Edad', required=True)
     curp = fields.Char('Curp',size=18)
+    
     ##Relacionales
 
     partner_id = fields.Many2one('res.partner','Escuela')
@@ -32,23 +35,13 @@ class academy_student(models.Model):
         'student_id',
         'Calificaciones')
     
-    country = fields.Many2one('res.country','Pais',
-                                related='partner_id.country_id')
-
+    country = fields.Many2one('res.country','Pais', related='partner_id.country_id')
 
     @api.one
     @api.constrains('curp')
     def _check_lines(self):
         if len(self.curp) < 18:
             raise exceptions.ValidationError("Curp debe ser de 18 caracteres")
-
-    _order ='name'
-
-    _defaults = {
-    	'active' : True,
-    	'state' : 'draf',
-    	}
-
     ###Metodo de escritura
     @api.multi
     def write(self, values):
@@ -68,6 +61,24 @@ class academy_student(models.Model):
                 'company_type': 'student',
                 'student_id': res['id'],
                 }
+        print (vals_to_partner)
         partner_id = partner_obj.create(vals_to_partner)
         print("===>partner_id", partner_id)
         return res
+
+    @api.multi
+    def unlink(self):
+        partner_obj = self.env['res.partner']
+        partner_ids = partner_obj.search([('student','=',self.id)])
+        print ("Partnet ##### >>>>>",partner_ids )
+        if partner_ids:
+            for partner in partner_ids:
+                partner.unlink()
+        res = super(academy_student, self).unlink()
+        return res
+
+    _order = 'name'
+    _defaults = {
+        'active' : True,
+        'state' : 'draf',
+        }
